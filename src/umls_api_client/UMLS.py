b12f83ef51351878efec3d1763d25dc69b038a6f
Naveen Jayakody <naveenjayakody12@gmail.com>
2023-11-18 04:02:24 +0800

    rate limit enforcer added

--- src/umls_api_client/UMLS.py
+++ src/umls_api_client/UMLS.py
@@ -1,20 +1,36 @@
 import json
 import requests
-
+import time
 
 class UMLS:
-    def __init__(self, api_key):
-        self.__api_key = api_key
-        self.__base_url = "https://uts-ws.nlm.nih.gov/rest"
-
+    def __init__(self, api_key, requests_per_second=15):
+        self._api_key = api_key
+        self._base_url = "https://uts-ws.nlm.nih.gov/rest"
+        self._requests_per_second = requests_per_second
+        self._counter = 0
+        self._reset_time = time.time() + 1.0
+    
+    def _wait_for_rate_limit(self):
+        if(self._counter == self._requests_per_second):
+            #If time elapsed is within the second, ie the second is not over yet
+            current_time = time.time()
+            if(current_time < self._reset_time):
+                print('sleep')
+                time.sleep(self._reset_time - current_time)
+                print('up')
+            else:
+                self._counter = 0
+            self._reset_time = time.time() + 1
+        else:
+            self._counter += 1
    
     def retrieve_cuis(self, query, version='current',inputType='atom',includeObsolete=False,includeSuppressible=False,returnIdType='concept',sabs=[],
                      searchType='words',partialSearch=False,pageNumber=1,pageSize=25):
-        search_endpoint = f"{self.__base_url}/search/{version}"
+        search_endpoint = f"{self._base_url}/search/{version}"
 
         params = {
             "string": query,
-            "apiKey": self.__api_key,
+            "apiKey": self._api_key,
             "inputType":inputType,
             "searchType":searchType,
             "pageNumber":pageNumber,
@@ -44,6 +60,7 @@ class UMLS:
             params["sabs"] = sabsString
 
         headers = {"Content-Type": "application/x-www-form-urlencoded"}
+        self._wait_for_rate_limit()
         response = requests.get(search_endpoint, params=params,headers=headers)
         data = response.json()
 
@@ -53,10 +70,11 @@ class UMLS:
             raise Exception(f"Search failed. Status code: {response.status_code}")
         
     def retrieve_cui_info(self, cui, version='current'):
-        search_endpoint = f"{self.__base_url}/content/{version}/CUI/{cui}"
-        params = {"apiKey":self.__api_key}
+        search_endpoint = f"{self._base_url}/content/{version}/CUI/{cui}"
+        params = {"apiKey":self._api_key}
 
         headers = {"Content-Type": "application/x-www-form-urlencoded"}
+        self._wait_for_rate_limit()
         response = requests.get(search_endpoint, params=params,headers=headers)
         data = response.json()
 
@@ -70,11 +88,11 @@ class UMLS:
 
     def retrieve_cui_atoms(self, cui, version='current', preferred=False,includeObsolete=True,includeSuppressible=True,sabs=[],language='',ttys=[]
                           ,pageNumber=1,pageSize=25):
-        search_endpoint = f"{self.__base_url}/content/{version}/CUI/{cui}/atoms"
+        search_endpoint = f"{self._base_url}/content/{version}/CUI/{cui}/atoms"
         if preferred:
             search_endpoint += "/"+"preferred"
         params = {
-            "apiKey": self.__api_key,
+            "apiKey": self._api_key,
             "pageNumber":pageNumber,
             "pageSize":pageSize,
         }
@@ -106,6 +124,7 @@ class UMLS:
 
 
         headers = {"Content-Type": "application/x-www-form-urlencoded"}
+        self._wait_for_rate_limit()
         response = requests.get(search_endpoint, params=params,headers=headers)
         data = response.json()
 
@@ -117,10 +136,10 @@ class UMLS:
 
     
     def retrieve_cui_definitions(self, cui, version='current',sabs=[], pageNumber=1, pageSize=25):
-        search_endpoint = f"{self.__base_url}/content/{version}/CUI/{cui}/definitions"
+        search_endpoint = f"{self._base_url}/content/{version}/CUI/{cui}/definitions"
         
         params = {
-            "apiKey": self.__api_key,
+            "apiKey": self._api_key,
             "pageNumber":pageNumber,
             "pageSize":pageSize,
         }
@@ -133,6 +152,7 @@ class UMLS:
 
 
         headers = {"Content-Type": "application/x-www-form-urlencoded"}
+        self._wait_for_rate_limit()
         response = requests.get(search_endpoint, params=params,headers=headers)
         data = response.json()
 
@@ -144,9 +164,9 @@ class UMLS:
 
     def retrieve_cui_relations(self, cui, version='current', includeRelationLabels=[],includeAdditionalRelationLabels=[],includeObsolete=False,includeSuppressible=False,sabs=[]
                           ,pageNumber=1,pageSize=25):
-        search_endpoint = f"{self.__base_url}/content/{version}/CUI/{cui}/relations"
+        search_endpoint = f"{self._base_url}/content/{version}/CUI/{cui}/relations"
         params = {
-            "apiKey": self.__api_key,
+            "apiKey": self._api_key,
             "pageNumber":pageNumber,
             "pageSize":pageSize,
         }
@@ -181,6 +201,7 @@ class UMLS:
 
 
         headers = {"Content-Type": "application/x-www-form-urlencoded"}
+        self._wait_for_rate_limit()
         response = requests.get(search_endpoint, params=params,headers=headers)
         data = response.json()
 
@@ -191,12 +212,13 @@ class UMLS:
         
     
     def retrieve_source_asserted_id_info(self,id,source,version='current'):
-        search_endpoint = f"{self.__base_url}/content/{version}/source/{source}/{id}"
+        search_endpoint = f"{self._base_url}/content/{version}/source/{source}/{id}"
         params = {
-            "apiKey": self.__api_key,
+            "apiKey": self._api_key,
         }
 
         headers = {"Content-Type": "application/x-www-form-urlencoded"}
+        self._wait_for_rate_limit()
         response = requests.get(search_endpoint, params=params,headers=headers)
         data = response.json()
 
@@ -208,12 +230,13 @@ class UMLS:
 
     
     def retrieve_source_asserted_id_atoms(self,id,source,version='current'):
-        search_endpoint = f"{self.__base_url}/content/{version}/source/{source}/{id}/atoms"
+        search_endpoint = f"{self._base_url}/content/{version}/source/{source}/{id}/atoms"
         params = {
-            "apiKey": self.__api_key,
+            "apiKey": self._api_key,
         }
 
         headers = {"Content-Type": "application/x-www-form-urlencoded"}
+        self._wait_for_rate_limit()
         response = requests.get(search_endpoint, params=params,headers=headers)
         data = response.json()
 
@@ -224,14 +247,15 @@ class UMLS:
     
 
     def retrieve_source_asserted_id_parents(self,id,source,version='current',pageNumber=1,pageSize=25):
-        search_endpoint = f"{self.__base_url}/content/{version}/source/{source}/{id}/parents"
+        search_endpoint = f"{self._base_url}/content/{version}/source/{source}/{id}/parents"
         params = {
-            "apiKey": self.__api_key,
+            "apiKey": self._api_key,
             "pageNumber":pageNumber,
             "pageSize":pageSize,
         }
 
         headers = {"Content-Type": "application/x-www-form-urlencoded"}
+        self._wait_for_rate_limit()
         response = requests.get(search_endpoint, params=params,headers=headers)
         data = response.json()
 
@@ -241,14 +265,15 @@ class UMLS:
             raise Exception(f"Search failed. Status code: {response.status_code}")
     
     def retrieve_source_asserted_id_children(self,id,source,version='current',pageNumber=1,pageSize=25):
-        search_endpoint = f"{self.__base_url}/content/{version}/source/{source}/{id}/children"
+        search_endpoint = f"{self._base_url}/content/{version}/source/{source}/{id}/children"
         params = {
-            "apiKey": self.__api_key,
+            "apiKey": self._api_key,
             "pageNumber":pageNumber,
             "pageSize":pageSize,
         }
 
         headers = {"Content-Type": "application/x-www-form-urlencoded"}
+        self._wait_for_rate_limit()
         response = requests.get(search_endpoint, params=params,headers=headers)
         data = response.json()
 
@@ -258,14 +283,15 @@ class UMLS:
             raise Exception(f"Search failed. Status code: {response.status_code}")
         
     def retrieve_source_asserted_id_ancestors(self,id,source,version='current',pageNumber=1,pageSize=25):
-        search_endpoint = f"{self.__base_url}/content/{version}/source/{source}/{id}/ancestors"
+        search_endpoint = f"{self._base_url}/content/{version}/source/{source}/{id}/ancestors"
         params = {
-            "apiKey": self.__api_key,
+            "apiKey": self._api_key,
             "pageNumber":pageNumber,
             "pageSize":pageSize,
         }
 
         headers = {"Content-Type": "application/x-www-form-urlencoded"}
+        self._wait_for_rate_limit()
         response = requests.get(search_endpoint, params=params,headers=headers)
         data = response.json()
 
@@ -275,14 +301,15 @@ class UMLS:
             raise Exception(f"Search failed. Status code: {response.status_code}")
     
     def retrieve_source_asserted_id_descendants(self,id,source,version='current',pageNumber=1,pageSize=25):
-        search_endpoint = f"{self.__base_url}/content/{version}/source/{source}/{id}/descendants"
+        search_endpoint = f"{self._base_url}/content/{version}/source/{source}/{id}/descendants"
         params = {
-            "apiKey": self.__api_key,
+            "apiKey": self._api_key,
             "pageNumber":pageNumber,
             "pageSize":pageSize,
         }
 
         headers = {"Content-Type": "application/x-www-form-urlencoded"}
+        self._wait_for_rate_limit()
         response = requests.get(search_endpoint, params=params,headers=headers)
         data = response.json()
 
@@ -294,9 +321,9 @@ class UMLS:
     
     def retrieve_source_asserted_id_relations(self,id,source,version='current',pageNumber=1,pageSize=25,
                                               includeRelationLabels=[],includeAdditionalRelationLabels=[],includeObsolete=False,includeSuppressible=False):
-        search_endpoint = f"{self.__base_url}/content/{version}/source/{source}/{id}/relations"
+        search_endpoint = f"{self._base_url}/content/{version}/source/{source}/{id}/relations"
         params = {
-            "apiKey": self.__api_key,
+            "apiKey": self._api_key,
             "pageNumber":pageNumber,
             "pageSize":pageSize,
         }
@@ -325,6 +352,7 @@ class UMLS:
 
 
         headers = {"Content-Type": "application/x-www-form-urlencoded"}
+        self._wait_for_rate_limit()
         response = requests.get(search_endpoint, params=params,headers=headers)
         data = response.json()
 
@@ -335,9 +363,9 @@ class UMLS:
         
     
     def retrieve_source_asserted_id_attributes(self,id,source,version='current',pageNumber=1,pageSize=25,includeAttributeNames=[]):
-        search_endpoint = f"{self.__base_url}/content/{version}/source/{source}/{id}/attributes"
+        search_endpoint = f"{self._base_url}/content/{version}/source/{source}/{id}/attributes"
         params = {
-            "apiKey": self.__api_key,
+            "apiKey": self._api_key,
             "pageNumber":pageNumber,
             "pageSize":pageSize,
         }
@@ -350,6 +378,7 @@ class UMLS:
         
       
         headers = {"Content-Type": "application/x-www-form-urlencoded"}
+        self._wait_for_rate_limit()
         response = requests.get(search_endpoint, params=params,headers=headers)
         data = response.json()
 
@@ -359,13 +388,14 @@ class UMLS:
             raise Exception(f"Search failed. Status code: {response.status_code}")
     
     def retrieve_tui_info(self,id,version='current'):
-        search_endpoint = f"{self.__base_url}/semantic-network/{version}/TUI/{id}"
+        search_endpoint = f"{self._base_url}/semantic-network/{version}/TUI/{id}"
         params = {
-            "apiKey": self.__api_key,
+            "apiKey": self._api_key,
         }    
             
       
         headers = {"Content-Type": "application/x-www-form-urlencoded"}
+        self._wait_for_rate_limit()
         response = requests.get(search_endpoint, params=params,headers=headers)
         data = response.json()
 
@@ -376,9 +406,9 @@ class UMLS:
     
     
     def crosswalk_vocabs_using_cuis(self,id,source,version='current',targetSource=[],includeObsolete=False,pageNumber=1,pageSize=25):
-        search_endpoint = f"{self.__base_url}/crosswalk/{version}/source/{source}/{id}"
+        search_endpoint = f"{self._base_url}/crosswalk/{version}/source/{source}/{id}"
         params = {
-            "apiKey": self.__api_key,
+            "apiKey": self._api_key,
             "pageNumber":pageNumber,
             "pageSize":pageSize,
         }    
@@ -395,6 +425,7 @@ class UMLS:
             params["includeObsolete"] = 'false'
       
         headers = {"Content-Type": "application/x-www-form-urlencoded"}
+        self._wait_for_rate_limit()
         response = requests.get(search_endpoint, params=params,headers=headers)
         data = response.json()
 
@@ -404,6 +435,7 @@ class UMLS:
             raise Exception(f"Search failed. Status code: {response.status_code}")
 
 
+    #def retrieve_tui_for_id(self, id):
 
     
 
