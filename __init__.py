5e72919ec97b61bad29deb6529f7010b29024789
Naveen Jayakody <naveenjayakody12@gmail.com>
2023-11-17 08:19:34 +0800

    package

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from .umls import UMLS
---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='umls-api',  
+    version='0.1',
+    description='Class to interact with the UMLS API',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Naveen Jayakody',
+    author_email='naveenjayakody12@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'json'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
